825b9306f442b5704e383276a83e62ed440901fe
Loic Alain Royer <royerloic@gmail.com>
2013-09-27 19:38:49 +0200

    added method to laser device interface

--- src/device/LaserDevice.java
+++ src/device/LaserDevice.java
@@ -3,20 +3,24 @@ package device;
 import variable.booleanv.BooleanVariable;
 import variable.doublev.DoubleVariable;
 
-public interface LaserDevice extends NamedDeviceInterface,VirtualDeviceInterface
+public interface LaserDevice extends
+														NamedDeviceInterface,
+														VirtualDeviceInterface
 {
 
 	public int getWavelengthInNanoMeter();
 
 	public void setTargetPowerInMilliWatt(double pTargetPowerinMilliWatt);
-	
+
 	public void setTargetPowerInPercent(double pTargetPowerInPercent);
 
 	public double getMaxPowerInMilliWatt();
-	
+
 	public double getCurrentPowerInMilliWatt();
-	
+
 	public BooleanVariable getLaserOnVariable();
 
 	public DoubleVariable getTargetPowerInMilliWattVariable();
+
+	public DoubleVariable getCurrentPowerInMilliWattVariable();
 }
