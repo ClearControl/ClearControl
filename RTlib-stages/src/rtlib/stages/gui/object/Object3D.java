4a40ff0ed6459a677e05b5ef34e3ceafae1be055
HongKee Moon <hkmoon@me.com>
2016-03-08 17:43:26 +0100

    Add new 3D based stage control

---
+++ RTlib-stages/src/rtlib/stages/gui/object/Object3D.java
@@ -0,0 +1,19 @@
+package rtlib.stages.gui.object;
+
+import javafx.scene.Group;
+
+/**
+ * Base class for 3D object
+ */
+public abstract class Object3D extends Group
+{
+	private static final double MODEL_SCALE_FACTOR = 10;
+
+	public Group buildScene() {
+		this.setScaleX(MODEL_SCALE_FACTOR);
+		this.setScaleY(MODEL_SCALE_FACTOR);
+		this.setScaleZ(MODEL_SCALE_FACTOR);
+
+		return new Group(this);
+	}
+}
