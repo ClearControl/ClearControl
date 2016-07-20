00eb5fbf3282a4a35db11e3e1bc3b7c793874b77
Loic Royer <royer@mpi-cbg.de>
2016-07-20 19:03:35 +0200

    improved servo iris control

--- src/clearcontrol/hardware/optomech/servoiris/SCX18ServoIris/SCX18ServoIris.ino
+++ src/clearcontrol/hardware/optomech/servoiris/SCX18ServoIris/SCX18ServoIris.ino
@@ -1,12 +1,5 @@
 /*
-
- Designer Systems DS-RGBW Shield Arduino Demonstrator
- 
- Requires Arduino Duemilanove or MEGA or NANO boards
- 
- DS-RGBW.S [A0 & A1 jumpers ON] connected to I2C interface (ANALOG IN 4 [SDA] & 5 [SCL])
- 
- RGBW_Application.ino	Date: 24/6/13	Version: 1.00 
+  SCX18 Servo Iris Controller
 
  */
 
@@ -29,62 +22,51 @@ void setup()
   digitalWrite(A0, LOW);
   pinMode(A1, INPUT);
 
+  for (int r=0; r<3; r++)
+  {
+    for(float i=0; i<=512; i++)
+    {
+      //Serial.println(i, DEC);
+      setAllPositionInt(i,512);
+  
+      trigger();
+      
+      delay(1);
+    }
+  
+    for(float i=512; i>=0; i--)
+    {
+      //Serial.println(i, DEC);
+      setAllPositionInt(i,512);
+      trigger();
+      
+      delay(1);
+    }/**/
+  }
+
 }
+
+int lastValue=-100;
 
 // Main code
 void loop()
 {
 
-  int sensorValue = analogRead(A1);
-  float f = (1.0f*sensorValue)/1024;
+  const int sensorValue = analogRead(A1);
+  const int value = scaleValue(sensorValue,1024);
 
-  Serial.println(sensorValue, DEC);
-  setPositionInt(0,sensorValue,1024);
-  setPositionInt(1,sensorValue,1024);
-  setPositionInt(2,sensorValue,1024);
-  setPositionInt(3,sensorValue,1024);
-  trigger();
-  //delay(30);
-  
-  
-  // Channel 1
-
-   /* 
-    WriteRegister(1, 34);
-    WriteRegister(2, 128);
-    WriteRegister(37, 0x00);
-    delay(10000);
-    /**/
-
-// [34,216]
-
-  
-  for(float i=0; i<=f; i+=0.01)
+  if(abs(value-lastValue)>1)
   {
-    //Serial.println(i, DEC);
-    setAllPositionFloat(i);
-
+    setPositionInt(0,value);
+    setPositionInt(1,value);
+    setPositionInt(2,value);
+    setPositionInt(3,value);
     trigger();
-    
-    delay(10);
+    lastValue = value;
+    //Serial.println(value, DEC);
+    delay(1);
   }
 
-
-  for(float i=f; i>=0; i-=0.01)
-  {
-    //Serial.println(i, DEC);
-    setAllPositionFloat(i);
-    trigger();
-    
-    delay(10);
-  }/**/
-
-/*
-  setPosition(1,0);
-  delay(200);
-  setPosition(1,1);
-  delay(200);/**/
-  
 }
 
 
@@ -99,14 +81,9 @@ void configure(byte pIndex, bool pOn, bool pSoftStart, bool pSpeedControl, byte 
   writeRegister(37, 0x00);
 }
 
-void setPositionFloat(byte pIndex, float pPos) 
+void setAllPositionInt(int pNum, int pDenom) 
 {
-  writeRegister(2*pIndex+1,(int)(pPos*(180-34)+34));
-}
-
-void setAllPositionFloat(float pPos) 
-{
-  int value = (int)(pPos*(180-34)+34);
+  int value = scaleValue(pNum,pDenom);
   writeRegister(1,value);
   writeRegister(3,value);
   writeRegister(5,value);
@@ -115,10 +92,20 @@ void setAllPositionFloat(float pPos)
 
 void setPositionInt(byte pIndex, int pNum, int pDenom) 
 {
-  int value = (int)((pNum*(180L-34L))/pDenom+34L);
+  int value = scaleValue(pNum,pDenom);
   //Serial.print("value=");
   //Serial.println(value, DEC);
   writeRegister(2*pIndex+1,value);
+}
+
+inline int scaleValue(int value, int scale)
+{
+  return  (int)((value*(180L-34L))/scale+34L);
+}
+
+void setPositionInt(byte pIndex, int pVal) 
+{
+  writeRegister(2*pIndex+1,pVal);
 }
 
 void trigger() 
@@ -133,5 +120,9 @@ void writeRegister(byte Register, byte Value)
   Wire.write(Value);
   Wire.endTransmission();				      
 }
+
+
+        
+
 
 
