189f9a5561e0874d00a6d979e79888a251073a76
HongKee Moon <hkmoon@me.com>
2016-08-22 10:55:23 +0200

    LightSheetAcquisitionStateInterface temporarily in order to test other stuffs
    Dummy methods are created InterpolationTables according to the usages.
    We will remove LightSheetAcquisitionStateInterface later.
    Please, do not use /develop branch for a while until we push the stable version.

--- src/clearcontrol/device/change/ChangeListeningBase.java
+++ src/clearcontrol/device/change/ChangeListeningBase.java
@@ -1,5 +1,7 @@
 package clearcontrol.device.change;
 
+import clearcontrol.core.math.interpolation.SplineInterpolationTable;
+
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
@@ -43,6 +45,68 @@ public abstract class ChangeListeningBase<E> implements HasChangeListenerInterfa
 			lListener.changed(pEvent);
 		}
 	}
-	
-	
+
+	public abstract void addAtControlPlaneIA( int pControlPlaneIndex, int pLightSheetIndex, double lCorrection );
+
+	public abstract void setAtControlPlaneIP( int czi, int l, double v );
+
+	public abstract double getAtControlPlaneIP( int i, int l );
+
+	public abstract void setAtControlPlaneIW( int pControlPlaneIndex, int pLightSheetIndex, Double aDouble );
+
+	public abstract void setAtControlPlaneIX( int pControlPlaneIndex, int pLightSheetIndex, Double aDouble );
+
+	public abstract void addAtControlPlaneIZ( int pControlPlaneIndex, int pLightSheetIndex, double lCorrection );
+
+	public abstract int getNumberOfDevicesDZ();
+
+	public abstract double getAtControlPlaneDZ( int czi, int d );
+
+	public abstract int getNumberOfDevicesIX();
+
+	public abstract double getAtControlPlaneIX( int czi, int i );
+
+	public abstract double getAtControlPlaneIY( int czi, int i );
+
+	public abstract double getAtControlPlaneIZ( int czi, int i );
+
+	public abstract double getAtControlPlaneIA( int czi, int i );
+
+	public abstract double getAtControlPlaneIB( int czi, int i );
+
+	public abstract double getAtControlPlaneIW( int czi, int i );
+
+	public abstract double getAtControlPlaneIH( int czi, int i );
+
+	public abstract SplineInterpolationTable getDZTable();
+
+	public abstract SplineInterpolationTable getIXTable();
+
+	public abstract SplineInterpolationTable getIYTable();
+
+	public abstract int getNumberOfDevicesIY();
+
+	public abstract SplineInterpolationTable getIZTable();
+
+	public abstract int getNumberOfDevicesIZ();
+
+	public abstract SplineInterpolationTable getIATable();
+
+	public abstract int getNumberOfDevicesIA();
+
+	public abstract SplineInterpolationTable getIBTable();
+
+	public abstract int getNumberOfDevicesIB();
+
+	public abstract SplineInterpolationTable getIWTable();
+
+	public abstract int getNumberOfDevicesIW();
+
+	public abstract SplineInterpolationTable getIHTable();
+
+	public abstract int getNumberOfDevicesIH();
+
+	public abstract SplineInterpolationTable getIPTable();
+
+	public abstract int getNumberOfDevicesIP();
 }
--- src/clearcontrol/hardware/cameras/devices/andorzyla/demo/AndorZylaCameraDemo.java
+++ src/clearcontrol/hardware/cameras/devices/andorzyla/demo/AndorZylaCameraDemo.java
@@ -1,38 +1,38 @@
-package clearcontrol.hardware.cameras.devices.andorzyla.demo;
-
-import static org.junit.Assert.assertTrue;
-
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicLong;
-
-import org.junit.Test;
-
-import clearcontrol.core.variable.Variable;
-import clearcontrol.gui.video.video2d.videowindow.VideoWindow;
-import clearcontrol.hardware.cameras.devices.andorzyla.AndorZylaStackCamera;
-import clearcontrol.hardware.cameras.devices.orcaflash4.OrcaFlash4StackCamera;
-import clearcontrol.stack.ContiguousOffHeapPlanarStackFactory;
-import clearcontrol.stack.StackInterface;
-import clearcontrol.stack.StackRequest;
-import coremem.recycling.BasicRecycler;
-import coremem.types.NativeTypeEnum;
-
-import andorsdkj.*;
-
-public class AndorZylaCameraDemo
-{
-
-	
-		@Test
-		public void testAndorZylaStackCameraOpenAndClose() throws Exception
-		{
-			// initializing andor environment
-			AndorSdkJ lAndorEnv = new AndorSdkJ();
-			AndorZylaStackCamera lZylaOne = new AndorZylaStackCamera(0);
-			lZylaOne.stop();
-			lAndorEnv.close();
-			
-		}
-}
+//package clearcontrol.hardware.cameras.devices.andorzyla.demo;
+//
+//import static org.junit.Assert.assertTrue;
+//
+//import java.io.IOException;
+//import java.util.concurrent.ExecutionException;
+//import java.util.concurrent.Future;
+//import java.util.concurrent.atomic.AtomicLong;
+//
+//import org.junit.Test;
+//
+//import clearcontrol.core.variable.Variable;
+//import clearcontrol.gui.video.video2d.videowindow.VideoWindow;
+//import clearcontrol.hardware.cameras.devices.andorzyla.AndorZylaStackCamera;
+//import clearcontrol.hardware.cameras.devices.orcaflash4.OrcaFlash4StackCamera;
+//import clearcontrol.stack.ContiguousOffHeapPlanarStackFactory;
+//import clearcontrol.stack.StackInterface;
+//import clearcontrol.stack.StackRequest;
+//import coremem.recycling.BasicRecycler;
+//import coremem.types.NativeTypeEnum;
+//
+//import andorsdkj.*;
+//
+//public class AndorZylaCameraDemo
+//{
+//
+//
+//		@Test
+//		public void testAndorZylaStackCameraOpenAndClose() throws Exception
+//		{
+//			// initializing andor environment
+//			AndorSdkJ lAndorEnv = new AndorSdkJ();
+//			AndorZylaStackCamera lZylaOne = new AndorZylaStackCamera(0);
+//			lZylaOne.stop();
+//			lAndorEnv.close();
+//
+//		}
+//}
