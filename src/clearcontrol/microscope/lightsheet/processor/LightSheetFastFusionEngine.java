15da81653a1b8477862b06bfb6c70f24c4eb0393
Loic Alain Royer <royerloic@gmail.com>
2017-05-19 17:40:46 +0200

    fixed aspect ratio in display when downscaling

--- src/clearcontrol/microscope/lightsheet/processor/LightSheetFastFusionEngine.java
+++ src/clearcontrol/microscope/lightsheet/processor/LightSheetFastFusionEngine.java
@@ -112,7 +112,7 @@ public class LightSheetFastFusionEngine extends FastFusionEngine
     if (isRegistration())
     {
 
-      if (mDownscale)
+      if (isDownscale())
       {
         addTask(new DownsampleXYbyHalfTask("C0L0", "C0L0d"));
         addTask(new DownsampleXYbyHalfTask("C0L1", "C0L1d"));
@@ -179,7 +179,7 @@ public class LightSheetFastFusionEngine extends FastFusionEngine
     else
     {
 
-      if (mDownscale)
+      if (isDownscale())
       {
         addTask(new DownsampleXYbyHalfTask("C0L0", "C0L0d"));
         addTask(new DownsampleXYbyHalfTask("C0L1", "C0L1d"));
@@ -227,7 +227,7 @@ public class LightSheetFastFusionEngine extends FastFusionEngine
 
   protected void setupFourLightsheetsOneDetectionArm()
   {
-    if (mDownscale)
+    if (isDownscale())
     {
       addTask(new DownsampleXYbyHalfTask("C0L0", "C0L0d"));
       addTask(new DownsampleXYbyHalfTask("C0L1", "C0L1d"));
@@ -255,7 +255,7 @@ public class LightSheetFastFusionEngine extends FastFusionEngine
   {
     if (isRegistration())
     {
-      if (mDownscale)
+      if (isDownscale())
       {
         addTask(new DownsampleXYbyHalfTask("C0L0", "C0L0d"));
         addTask(new DownsampleXYbyHalfTask("C0L1", "C0L1d"));
@@ -328,7 +328,7 @@ public class LightSheetFastFusionEngine extends FastFusionEngine
 
   protected void setupTwoLightSheetsOneDetectionArm()
   {
-    if (mDownscale)
+    if (isDownscale())
     {
       addTask(new DownsampleXYbyHalfTask("C0L0", "C0L0d"));
       addTask(new DownsampleXYbyHalfTask("C0L1", "C0L1d"));
@@ -350,7 +350,7 @@ public class LightSheetFastFusionEngine extends FastFusionEngine
   {
     if (isRegistration())
     {
-      if (mDownscale)
+      if (isDownscale())
       {
         addTask(new DownsampleXYbyHalfTask("C0L0", "C0L0d"));
         addTask(new DownsampleXYbyHalfTask("C1L0", "C1L0d"));
@@ -390,7 +390,7 @@ public class LightSheetFastFusionEngine extends FastFusionEngine
     }
     else
     {
-      if (mDownscale)
+      if (isDownscale())
       {
         addTask(new DownsampleXYbyHalfTask("C0L0", "C0L0d"));
         addTask(new DownsampleXYbyHalfTask("C1L0", "C1L0d"));
@@ -413,7 +413,7 @@ public class LightSheetFastFusionEngine extends FastFusionEngine
 
   protected void setupOneLightsheetOneDetectionArm()
   {
-    if (mDownscale)
+    if (isDownscale())
     {
       addTask(new DownsampleXYbyHalfTask("C0L0", "fused"));
     }
@@ -546,6 +546,27 @@ public class LightSheetFastFusionEngine extends FastFusionEngine
   public void setRegistration(boolean pRegistration)
   {
     mRegistration = pRegistration;
+  }
+
+  /**
+   * Returns true if downscale by a factor 2 along XY is active
+   * 
+   * @return true if downscale isactive
+   */
+  public boolean isDownscale()
+  {
+    return mDownscale;
+  }
+
+  /**
+   * Sets whether to downscale by a factor 2 along XY
+   * 
+   * @param pDownscale
+   *          downscale on or off
+   */
+  public void setDownscale(boolean pDownscale)
+  {
+    mDownscale = pDownscale;
   }
 
 }
