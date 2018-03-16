a75e2976a2f0a4a86ac2fcc678c3f702b822987e
haesleinhuepf <rhaase@mpi-cbg.de>
2018-03-16 15:35:52 +0100

    removed up-date code in interpolation table; values are calculated on the fly

--- src/clearcontrol/core/math/interpolation/LinearInterpolationTable.java
+++ src/clearcontrol/core/math/interpolation/LinearInterpolationTable.java
@@ -29,7 +29,7 @@ public class LinearInterpolationTable extends AbstractInterpolationTable
   @Override public double getInterpolatedValue(int pColumnIndex,
                                                double pX)
   {
-    ensureIsUpToDate();
+    //ensureIsUpToDate();
 
     double yA = getCeilRow(pX).getY(pColumnIndex);
     double yB = getFloorRow(pX).getY(pColumnIndex);
@@ -63,90 +63,6 @@ public class LinearInterpolationTable extends AbstractInterpolationTable
   public LinearInterpolationTable clone()
   {
     return new LinearInterpolationTable(this);
-  }
-
-  private void ensureIsUpToDate()
-  {
-    if (!isIsUpToDate())
-    {
-      final UnivariateInterpolator lUnivariateInterpolator =
-          new LinearInterpolator();
-      /*new LoessInterpolator(	max(0.25 * mTable.size(),
-      																																							3) / mTable.size(),
-      																																					0);/**/
-      mInterpolatingFunctionsList.clear();
-
-      final TDoubleArrayList x = new TDoubleArrayList();
-      final TDoubleArrayList y = new TDoubleArrayList();
-
-      for (int i = 0; i < mNumberOfColumns; i++)
-      {
-        x.clear();
-        y.clear();
-        for (final Row lRow : mTable)
-        {
-          double lValueY = lRow.getY(i);
-
-          if (!Double.isNaN(lValueY))
-          {
-            y.add(lValueY);
-            x.add(lRow.getX());
-          }
-        }
-
-        final double lMinX = getMinX();
-        final double lMaxX = getMaxX();
-
-        // this makes sure that we extrapolate outside of the actual range
-        final double lRangeWidth = 10000 * abs(lMaxX - lMinX);
-
-        if (x.size() >= 2)
-        {
-          x.insert(0, x.get(0) - lRangeWidth);
-          x.add(x.get(x.size() - 1) + lRangeWidth);
-        }
-
-        if (x.size() >= 2)
-        {
-          y.insert(0, y.get(0));
-          y.add(y.get(y.size() - 1));
-
-          y.set(0,
-                y.get(1) - lRangeWidth * ((y.get(1) - y.get(2))
-                                          / (x.get(1) - x.get(2))));
-          y.set(y.size() - 1,
-                y.get(y.size() - 2) + lRangeWidth
-                                      * ((y.get(y.size() - 2)
-                                          - y.get(y.size() - 3))
-                                         / (x.get(y.size() - 2)
-                                            - x.get(y.size() - 3))));
-        }
-
-        final UnivariateFunction lUnivariateFunction;
-
-        if (x.size() == 0)
-        {
-          lUnivariateFunction = new UnivariateAffineFunction(0, 0);
-        }
-        else if (x.size() <= 1)
-        {
-          lUnivariateFunction =
-              new UnivariateAffineFunction(0,
-                                           x.get(0));
-        }
-        else
-        {
-
-          lUnivariateFunction =
-              lUnivariateInterpolator.interpolate(x.toArray(),
-                                                  y.toArray());
-        }
-        mInterpolatingFunctionsList.add(lUnivariateFunction);
-
-      }
-
-      setUpToDate();
-    }
   }
 
 
