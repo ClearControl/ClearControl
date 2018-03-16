563fa105e087c5878bdeb9dd551b3b5a93a4acc9
haesleinhuepf <rhaase@mpi-cbg.de>
2018-03-16 14:57:03 +0100

    bugfix: make linear interpolation great (and working) again

--- src/clearcontrol/core/math/interpolation/AbstractInterpolationTable.java
+++ src/clearcontrol/core/math/interpolation/AbstractInterpolationTable.java
@@ -34,7 +34,6 @@ public abstract class AbstractInterpolationTable
   }
 
 
-
   /**
    * Returns the number of rows in the table
    *
--- src/clearcontrol/core/math/interpolation/LinearInterpolationTable.java
+++ src/clearcontrol/core/math/interpolation/LinearInterpolationTable.java
@@ -1,5 +1,14 @@
 package clearcontrol.core.math.interpolation;
 
+import clearcontrol.core.math.functions.UnivariateAffineFunction;
+import gnu.trove.list.array.TDoubleArrayList;
+import org.apache.commons.math3.analysis.UnivariateFunction;
+import org.apache.commons.math3.analysis.interpolation.LinearInterpolator;
+import org.apache.commons.math3.analysis.interpolation.SplineInterpolator;
+import org.apache.commons.math3.analysis.interpolation.UnivariateInterpolator;
+
+import static java.lang.Math.abs;
+
 /**
  * Author: Robert Haase (http://haesleinhuepf.net) at MPI CBG (http://mpi-cbg.de)
  * March 2018
@@ -20,15 +29,19 @@ public class LinearInterpolationTable extends AbstractInterpolationTable
   @Override public double getInterpolatedValue(int pColumnIndex,
                                                double pX)
   {
+    ensureIsUpToDate();
+
     double yA = getCeilRow(pX).getY(pColumnIndex);
     double yB = getFloorRow(pX).getY(pColumnIndex);
 
-    double dA = Math.abs(getCeilRow(pX).getX() - pX);
-    double dB = Math.abs(getFloorRow(pX).getX() - pX);
+    double dB = Math.abs(getCeilRow(pX).getX() - pX);
+    double dA = Math.abs(getFloorRow(pX).getX() - pX);
 
     double distance = dA + dB;
     double result = (yA * dA + yB * dB) / distance;
-
+    if (Double.isNaN(result)) {
+      return 0;
+    }
     return result;
   }
 
@@ -50,6 +63,90 @@ public class LinearInterpolationTable extends AbstractInterpolationTable
   public LinearInterpolationTable clone()
   {
     return new LinearInterpolationTable(this);
+  }
+
+  private void ensureIsUpToDate()
+  {
+    if (!isIsUpToDate())
+    {
+      final UnivariateInterpolator lUnivariateInterpolator =
+          new LinearInterpolator();
+      /*new LoessInterpolator(	max(0.25 * mTable.size(),
+      																																							3) / mTable.size(),
+      																																					0);/**/
+      mInterpolatingFunctionsList.clear();
+
+      final TDoubleArrayList x = new TDoubleArrayList();
+      final TDoubleArrayList y = new TDoubleArrayList();
+
+      for (int i = 0; i < mNumberOfColumns; i++)
+      {
+        x.clear();
+        y.clear();
+        for (final Row lRow : mTable)
+        {
+          double lValueY = lRow.getY(i);
+
+          if (!Double.isNaN(lValueY))
+          {
+            y.add(lValueY);
+            x.add(lRow.getX());
+          }
+        }
+
+        final double lMinX = getMinX();
+        final double lMaxX = getMaxX();
+
+        // this makes sure that we extrapolate outside of the actual range
+        final double lRangeWidth = 10000 * abs(lMaxX - lMinX);
+
+        if (x.size() >= 2)
+        {
+          x.insert(0, x.get(0) - lRangeWidth);
+          x.add(x.get(x.size() - 1) + lRangeWidth);
+        }
+
+        if (x.size() >= 2)
+        {
+          y.insert(0, y.get(0));
+          y.add(y.get(y.size() - 1));
+
+          y.set(0,
+                y.get(1) - lRangeWidth * ((y.get(1) - y.get(2))
+                                          / (x.get(1) - x.get(2))));
+          y.set(y.size() - 1,
+                y.get(y.size() - 2) + lRangeWidth
+                                      * ((y.get(y.size() - 2)
+                                          - y.get(y.size() - 3))
+                                         / (x.get(y.size() - 2)
+                                            - x.get(y.size() - 3))));
+        }
+
+        final UnivariateFunction lUnivariateFunction;
+
+        if (x.size() == 0)
+        {
+          lUnivariateFunction = new UnivariateAffineFunction(0, 0);
+        }
+        else if (x.size() <= 1)
+        {
+          lUnivariateFunction =
+              new UnivariateAffineFunction(0,
+                                           x.get(0));
+        }
+        else
+        {
+
+          lUnivariateFunction =
+              lUnivariateInterpolator.interpolate(x.toArray(),
+                                                  y.toArray());
+        }
+        mInterpolatingFunctionsList.add(lUnivariateFunction);
+
+      }
+
+      setUpToDate();
+    }
   }
 
 
