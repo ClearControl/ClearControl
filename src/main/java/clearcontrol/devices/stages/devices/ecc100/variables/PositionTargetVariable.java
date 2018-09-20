92cd090f48bd90e1916a7e7f63d7c3b3bd5395a9
Loic Royer <royerloic@gmail.com>
2018-09-20 16:27:29 -0700

    Update PositionTargetVariable.java
    fix

--- src/main/java/clearcontrol/devices/stages/devices/ecc100/variables/PositionTargetVariable.java
+++ src/main/java/clearcontrol/devices/stages/devices/ecc100/variables/PositionTargetVariable.java
@@ -6,7 +6,6 @@ import ecc100.ECC100Axis;
 public class PositionTargetVariable extends Variable<Double>
 {
 
-  private static final double cEpsilon = 5; // nm
   private final ECC100Axis mECC100Axis;
 
   public PositionTargetVariable(String pVariableName,
@@ -20,7 +19,7 @@ public class PositionTargetVariable extends Variable<Double>
   public Double setEventHook(Double pOldValue, Double pNewValue)
   {
     final double lValue = super.setEventHook(pOldValue, pNewValue);
-    mECC100Axis.goToPosition(pNewValue, cEpsilon);
+    mECC100Axis.goToPosition(lValue, cEpsilon);
     return lValue;
   }
 
